a937d02845dc06005a461caf7d9e18bf368c2c2a
chunza2542 <chunza2542@hotmail.com>
2019-02-16 08:39:45 +0700

    Map curry with location label

--- mapCurry.js
+++ mapCurry.js
@@ -1,9 +1,27 @@
 const fs = require("fs");
 
 const curries = JSON.parse(fs.readFileSync("./curry.json").toString());
-const locations = fs.readFileSync("./curry-with-location.csv").toString().split("\n");
+const curriesWithLocation = JSON.parse(fs.readFileSync("./curry-with-location.json").toString());
 
-const curryWithLocation = [];
-locations.forEach(curry => {
-    const token = curry.split(",");
+const search = (username) => curries.find(({name}) => name === username);
+
+const curriesWithLabel = curriesWithLocation.map(curry => {
+    const item = search(curry.name);
+    if (!item) {
+        return {}
+    }
+
+    const places = search(curry.name).location.split(",");
+    const location = curry.location.map((data, index) => ({
+        ...data,
+        label: places[index].trim(),
+    }))
+
+    return {
+        ...curry,
+        location,
+    }
 })
+
+fs.writeFileSync("./curry-with-location.json", JSON.stringify(curriesWithLabel, 2, 2))
+console.log("MAP CURRY DONE~")
